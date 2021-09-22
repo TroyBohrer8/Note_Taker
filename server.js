4a3012f2840fc48dffa6a7748323a3732610e1a7
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-09-22 12:19:28 -0400

    created GET route for notes page

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes pages
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 // GET Route for feedback page
 app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
