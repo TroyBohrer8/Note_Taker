77682e7e433beea1a3d7fc42e352bcf363de6f43
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-09-22 13:11:19 -0400

    created post route to append note to page

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const db = require('./db/db.json');
-const api = require('./routes/index.js');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { clog } = require("./middleware/clog");
+const db = require("./db/db.json");
+const api = require("./routes/index.js");
 
 const PORT = process.env.port || 3001;
 
@@ -15,28 +15,84 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use("/api", api);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for notes pages
-app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get("/feedback", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/pages/feedback.html"))
 );
 
+// POST Route to add notes
+app.post("/api/notes", (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      id: uuid(),
+      title,
+      text,
+    };
+
+    // Obtain existing notes
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNote = JSON.parse(data);
+
+        // Add a new note
+        parsedNote.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNote, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully added your note!")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json("Error in posting your note");
+  }
+});
+
 // Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/pages/404.html"))
 );
 
 app.listen(PORT, () =>
